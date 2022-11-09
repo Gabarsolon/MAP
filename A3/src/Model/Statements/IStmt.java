0d441b716ac131c0e2cae26f68b500cb4888b93b
917_Turcu_Remus <78847479+Gabarsolon@users.noreply.github.com>
2022-11-09 19:11:28 +0200

    readFile

--- A3/src/Model/Statements/IStmt.java
+++ A3/src/Model/Statements/IStmt.java
@@ -2,7 +2,6 @@ package Model.Statements;
 
 import Model.Exceptions.MyException;
 import Model.States.PrgState;
-
 public interface IStmt {
     public PrgState execute(PrgState state) throws MyException;
     public IStmt deepCopy();
